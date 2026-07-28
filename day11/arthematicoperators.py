7aa26d66f54a2297a2d6593218c4c7ba417b8fa3
Challagolla NandaKumar <Challagollanandakumar04@gmail.com>
2026-07-28 11:47:40 +0530

    learned arthematic operators

---
+++ day11/arthematicoperators.py
@@ -0,0 +1,21 @@
+a = 15
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("exponent:", num1 ** num2)
