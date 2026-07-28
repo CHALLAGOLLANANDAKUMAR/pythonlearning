e37c5af75b1ed52844a0cf13148edf718e2d0aab
Challagolla NandaKumar <Challagollanandakumar04@gmail.com>
2026-07-28 06:41:56 +0530

    sample program

---
+++ day4/sample1.py
@@ -0,0 +1,41 @@
+
+name = "Nanda Kumar"
+age = 20
+marks = 92.5
+college = "NRI Institute"
+course = "B.Tech IT"
+height = 5.8
+cgpa = 8.7
+student = True
+hosteller = False
+fees = 50000
+
+# Print values
+print(name)
+print(age)
+print(marks)
+print(college)
+print(course)
+print(height)
+print(cgpa)
+print(student)
+print(hosteller)
+print(fees)
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(college))
+print(type(course))
+print(type(height))
+print(type(cgpa))
+print(type(student))
+print(type(hosteller))
+print(type(fees))
+
+a = 10
+b = 20
+
+print("Before:", a, b)
+a, b = b, a
+print("After:", a, b)
